d628020088a347259c31c92db579b06edf526aa5
ardelon <akeskinw@gmail.com>
2022-05-23 07:28:37 +0300

    first commit

---
+++ notes.js
@@ -0,0 +1,22 @@
+
+
+const facilityData = {
+    name : "Maçka Parkı",
+    province : "İstanbul",
+    county : "Şişli",
+    neighborhood : "19 Mayıs",
+    attributes : {
+        branch : "Park",
+        facilityName : "Maçka Parkı",
+        groundType : "dirt",
+        lighting : true,
+        roof : false,
+
+    },
+    geometry : {
+        coordinates : [29,41],
+        type : "Point"
+    },
+    isExist : true
+}
+
